dd3497d72783964ca148db3784b69e9d48df950c
santacruz93-a11y <santacruz93@gmail.com>
2026-03-02 22:38:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
